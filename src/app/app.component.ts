271785081ba83cda77d2345328dc01970c1d34d7
Jon Zabala <j_zabalabur@fpzornotza.com>
2024-12-10 14:00:15 +0100

    Ariketa eginda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import listadePostres from 'src/assets/json/postres.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  postres: any= listadePostres;
   title = 'ejercicio7postres';
 }
